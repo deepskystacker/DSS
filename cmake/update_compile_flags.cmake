693480330170c2f0296870c048712a0053cda496
Tonk <tony__cook@hotmail.com>
2018-06-20 00:23:31 +0100

    The 64 bit release build is totally clean The 64 bit debug build as same linker warnings as reference hand build The 64 bit relwithdebuginfo is missing prebuilt components

--- cmake/update_compile_flags.cmake
+++ cmake/update_compile_flags.cmake
@@ -13,8 +13,59 @@ function (UPDATE_SOURCE_FILES_COMPILE_FLAGS SourceFiles CompileFlags)
    endforeach (filename)   
 endfunction (UPDATE_SOURCE_FILES_COMPILE_FLAGS)
 
-# This function appends the given link flags to all subsequent targets
 
+
+# These functions append the given compile flags to all subsequent targets of the indicated configuration
+
+  # All configs
+  
+function (UPDATE_COMPILE_FLAGS CompileFlags)
+   add_compile_options ("${CompileFlags}") 
+endfunction (UPDATE_COMPILE_FLAGS)
+
+  # Just release config
+  
+function (UPDATE_RELEASE_COMPILE_FLAGS CompileFlags)
+   add_compile_options ("$<$<CONFIG:RELEASE>:${CompileFlags}>")
+endfunction (UPDATE_RELEASE_COMPILE_FLAGS)
+
+  # Just relwithdebuginfo config
+  
+function (UPDATE_RELWITHDEBUGINFO_COMPILE_FLAGS CompileFlags)
+   add_compile_options ("$<$<CONFIG:RELWITHDEBUGINFO>:${CompileFlags}>")
+endfunction (UPDATE_RELWITHDEBUGINFO_COMPILE_FLAGS)
+
+  # Just debug config
+
+function (UPDATE_DEBUG_COMPILE_FLAGS LinkFlags)
+   add_compile_options ("$<$<CONFIG:DEBUG>:${CompileFlags}>")
+endfunction (UPDATE_DEBUG_COMPILE_FLAGS)
+
+
+
+
+# These functions append the given link flags to all subsequent targets of the indicated configuration
+
+  # All configs
+  
 function (UPDATE_LINK_FLAGS LinkFlags)
    set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${LinkFlags}" PARENT_SCOPE)
 endfunction (UPDATE_LINK_FLAGS)
+
+  # Just release config
+  
+function (UPDATE_RELEASE_LINK_FLAGS LinkFlags)
+   set (CMAKE_EXE_LINKER_FLAGS_RELEASE "${CMAKE_EXE_LINKER_FLAGS_RELEASE} ${LinkFlags}" PARENT_SCOPE)
+endfunction (UPDATE_RELEASE_LINK_FLAGS)
+
+  # Just relwithdebuginfo config
+  
+function (UPDATE_RELWITHDEBUGINFO_LINK_FLAGS LinkFlags)
+   set (CMAKE_EXE_LINKER_FLAGS_RELWITHDEBUGINFO "${CMAKE_EXE_LINKER_FLAGS_RELWITHDEBUGINFO} ${LinkFlags}" PARENT_SCOPE)
+endfunction (UPDATE_RELWITHDEBUGINFO_LINK_FLAGS)
+
+  # Just debug config
+
+function (UPDATE_DEBUG_LINK_FLAGS LinkFlags)
+   set (CMAKE_EXE_LINKER_FLAGS_DEBUG "${CMAKE_EXE_LINKER_FLAGS_DEBUG} ${LinkFlags}" PARENT_SCOPE)
+endfunction (UPDATE_DEBUG_LINK_FLAGS)
