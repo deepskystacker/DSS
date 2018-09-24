111584db30b63ee33e98077e70a4ad3bcd1ff101
laug <5089281+laug@users.noreply.github.com>
2018-09-24 16:09:41 +0900

    Cleaner fix for removing _USE macro.

--- Tools/StdString.h
+++ Tools/StdString.h
@@ -754,9 +754,9 @@ inline const Type& SSMAX(const Type& arg1, const Type& arg2)
 		#endif
 		#define SS_USE_FACET(loc, fac) std::use_facet<fac >(loc)
 
-	#elif defined(_MSC_VER )
+	//#elif defined(_MSC_VER )
 
-		#define SS_USE_FACET(loc, fac) std::use_facet<fac>(loc)
+	//	#define SS_USE_FACET(loc, fac) std::_USE(loc, fac)
 
 	// ...and
 	#elif defined(_RWSTD_NO_TEMPLATE_ON_RETURN_TYPE)
