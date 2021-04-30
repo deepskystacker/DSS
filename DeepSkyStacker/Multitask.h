08d4ae5fad14c5c7ae596ca3255ffddbedd4ca14
Martin Toeltsch <martin.toeltsch@toeltsch.at>
2021-04-30 11:58:55 +0200

    Remove everything from class CMultitask except static functions.

--- DeepSkyStacker/Multitask.h
+++ DeepSkyStacker/Multitask.h
@@ -7,24 +7,24 @@ constexpr UINT WM_MT_STOP		= WM_USER + 2;
 class CMultitask
 {
 protected:
-	CComAutoCriticalSection	m_CriticalSection;
-	std::vector<HANDLE>		m_vThreads;
-	std::vector<DWORD>		m_vThreadIds;
-	std::vector<HANDLE>		m_vEvents;
+	//CComAutoCriticalSection	m_CriticalSection;
+	//std::vector<HANDLE>		m_vThreads;
+	//std::vector<DWORD>		m_vThreadIds;
+	//std::vector<HANDLE>		m_vEvents;
 
 protected:
-	int GetNrThreads()
-	{
-		return static_cast<int>(m_vThreads.size());
-	};
+	//int GetNrThreads()
+	//{
+	//	return static_cast<int>(m_vThreads.size());
+	//};
 
 public:
 	CMultitask() = default;
 
-	virtual ~CMultitask()
-	{
-		CloseAllThreads();
-	};
+	//virtual ~CMultitask()
+	//{
+	//	CloseAllThreads();
+	//};
 
 	static int	GetNrProcessors(bool bReal = false);
 	static void	SetUseAllProcessors(bool bUseAll);
@@ -33,23 +33,23 @@ public:
 	static bool GetUseSimd();
 	static void SetUseSimd(const bool bUseSimd);
 
-	HANDLE GetThreadEvent(const DWORD dwThreadId)
-	{
-		for (size_t i = 0; i < m_vThreadIds.size(); i++)
-		{
-			if (dwThreadId == m_vThreadIds[i])
-				return m_vEvents[i];
-		};
-		return nullptr;
-	};
+	//HANDLE GetThreadEvent(const DWORD dwThreadId)
+	//{
+	//	for (size_t i = 0; i < m_vThreadIds.size(); i++)
+	//	{
+	//		if (dwThreadId == m_vThreadIds[i])
+	//			return m_vEvents[i];
+	//	};
+	//	return nullptr;
+	//};
 
-	void CloseAllThreads();
-	void StartThreads(int lNrThreads = 0);
-	HANDLE GetAvailableThread();
-	DWORD GetAvailableThreadId();
+	//void CloseAllThreads();
+	//void StartThreads(int lNrThreads = 0);
+	//HANDLE GetAvailableThread();
+	//DWORD GetAvailableThreadId();
 
-	virtual bool DoTask(HANDLE hEvent) = 0;
-	virtual bool Process() = 0;
+	//virtual bool DoTask(HANDLE hEvent) = 0;
+	//virtual bool Process() = 0;
 };
 
 #endif // __MULTITASK_H__
