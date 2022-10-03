8b83de4945b1c9d784954c8c2aaaa61b20ad7042
perdrix52 <david.partridge@perdrix.co.uk>
2022-10-03 17:44:54 +0100

    Comment out declartion of setOverlayPixmap

--- DeepSkyStacker/QLinearGradientCtrl.h
+++ DeepSkyStacker/QLinearGradientCtrl.h
@@ -1,10 +1,39 @@
-#if !defined(QLINEARGRADIENTCTRL_HEADER)
-#define QLINEARGRADIENTCTRL_HEADER
-
-#if _MSC_VER > 1000
 #pragma once
-#endif // _MSC_VER > 1000
-
+/****************************************************************************
+**
+** Copyright (C) 2020, 2022 David C. Partridge
+**
+** BSD License Usage
+** You may use this file under the terms of the BSD license as follows:
+**
+** "Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are
+** met:
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in
+**     the documentation and/or other materials provided with the
+**     distribution.
+**   * Neither the name of DeepSkyStacker nor the names of its
+**     contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+**
+**
+****************************************************************************/
 // QLinearGradientCtrl : header file
 //
 #include <zexcept.h>
@@ -26,7 +55,7 @@ class QString;
 #define GC_EDITPEG				6
 #define GC_CHANGE				7
 
-class QLinearGradientCtrlImpl;
+//class QLinearGradientCtrlImpl;
 
 /////////////////////////////////////////////////////////////////////////////
 // QLinearGradientCtrl window
@@ -37,19 +66,18 @@ class QLinearGradientCtrl : public QWidget
 	Q_PROPERTY(QLinearGradient gradient READ gradient WRITE setGradient)
 	Q_PROPERTY(int gradientWidth READ orientation WRITE setGradientWidth)
 	Q_PROPERTY(QLinearGradientCtrl::Orientation orientation READ orientation WRITE setOrientation)
-	Q_PROPERTY(int selectedIndex READ selectedIndex WRITE setSelectedIndex NOTIFY pegSelChanged)
+	Q_PROPERTY(int selectedPeg READ selected WRITE setSelected NOTIFY pegSelChanged)
 	Q_PROPERTY(QGradientStop selectedStop READ selectedStop)
 	Q_PROPERTY(bool pegsOnLeftOrBottom READ pegsOnLeftOrBottom WRITE setPegsOnLeftOrBottom)
 	Q_PROPERTY(bool pegsOnRightOrTop READ pegsOnRightOrTop WRITE setPegsOnRightOrTop)
 	Q_PROPERTY(bool showToolTips READ showToolTips WRITE setShowToolTips)
-	Q_PROPERTY(QString toolTipFormat READ toolTipFormat WRITE setToolTipFormat)
 
 typedef QWidget
 		Inherited;
 
 	// Construction
 public:
-	QLinearGradientCtrl(QWidget* parent= nullptr);
+	QLinearGradientCtrl(QWidget* parent= nullptr, QColor start = Qt::black, QColor end = Qt::white) ;
 	// BOOL Create(const RECT& rect, CWnd* pParentWnd, UINT nID);
 
 	virtual ~QLinearGradientCtrl();
@@ -63,7 +91,7 @@ public:
 
 	// Attributes
 public:
-	QLinearGradient gradient() { return m_Gradient; };
+	QLinearGradient& gradient() { return m_Gradient; };
 	QLinearGradientCtrl &
 		setGradient(QLinearGradient const& src);
 
@@ -75,8 +103,8 @@ public:
 	QLinearGradientCtrl &
 		setOrientation(Orientation orientation) { m_Orientation = orientation; return *this; };
 
-	int		selectedIndex() const { return selectedPeg; };
-	int		setSelectedIndex(int iSel);
+	int		selected() const { return selectedPeg; };
+	int		setSelected(int iSel);
 
 	QGradientStop selectedStop() const;
 
@@ -99,6 +127,7 @@ public:
 	void	deleteSelected(bool bUpdate);
 	int		moveSelected(qreal newpos, bool bUpdate);
 	QColor	setSelectedPegColour(QColor newColour, bool bUpdate);
+	void	setColorAt(double pos, QColor colour);
 
 	// Internals
 protected:
@@ -108,7 +137,8 @@ protected:
 	QLinearGradient	m_Gradient;
 	int			m_Width;
 
-	int			selectedPeg;
+	int	selectedPeg;
+	int lastSelectedPeg;
 	int			m_LastPos;
 	QPoint		m_MouseDown;
 
@@ -126,19 +156,21 @@ protected:
 	bool m_LeftDownSide;
 	bool m_RightUpSide;
 
-	void	addAnchorStops();
 	QColor	colourFromPoint(const QPoint & point);
+	QRect	gradientRect() const;
 	void	drawGradient(QPainter & painter);
 	void	drawEndPegs(QPainter & painter);
 	void	drawPeg(QPainter & painter, QPoint point, QColor colour, int direction);
 	void	drawPegs(QPainter & painter);
 	void	drawSelPeg(QPainter & painter, int peg);
 	void	drawSelPeg(QPainter & painter, QPoint point, int direction);
-	bool	isVertical();
+	bool	isVertical() const;
 	int		pointFromPos(qreal pos);
 	qreal	posFromPoint(int point);
-	int		getDrawWidth();
+	int		getDrawWidth() const;
+	QPolygon getPegPoly(int index);
 	void	getPegRect(int index, QRect *rect, bool right);
+	QRegion getPegRegion(short peg);
 	QRegion getPegRegion();
 	int		getPegIndent(int index);
 	int		setPeg(int index, QColor colour, qreal position);
@@ -169,5 +201,3 @@ protected:
 
 
 };
-
-#endif
