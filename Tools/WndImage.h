ad7bb4c7499dbc9e5f7045575eaf6e6c1f54de58
perdrix52 <david.partridge@perdrix.co.uk>
2022-10-18 12:15:39 +0100

    Move deletion of buffers from dtor to new OnDestroy mf() whici is invoked on WM_DESTROY.   Prevents invalid pointer problems when closing down

--- Tools/WndImage.h
+++ Tools/WndImage.h
@@ -246,6 +246,7 @@ protected:
 	afx_msg BOOL OnEraseBkgnd(CDC* pDC);
  	afx_msg void OnPaint();
 	afx_msg void OnSize(UINT nType, int cx, int cy);
+	afx_msg void OnDestroy();
 	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
 	afx_msg void OnRButtonDown(UINT nFlags, CPoint point);
 	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
