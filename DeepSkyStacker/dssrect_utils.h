ac2042a21e1554f21742c7a67c896fee7bf94377
Martin Toeltsch <martin.toeltsch@toeltsch.at>
2022-11-21 12:17:11 +0100

    Resolve linker problem with multiple instances.

--- DeepSkyStacker/dssrect_utils.h
+++ DeepSkyStacker/dssrect_utils.h
@@ -8,7 +8,7 @@ namespace DSS {
 	// This is a replacement for the old CPointExt::IsInRect(double fLeft, double fTop, double fRight, double fBottom)
 	// The check has been copied literally.
 	//
-	bool pointIsInRect(const QPointF& point, const double rectleft, const double recttop, const double rectright, const double rectbottom) noexcept
+	inline bool pointIsInRect(const QPointF& point, const double rectleft, const double recttop, const double rectright, const double rectbottom) noexcept
 	{
 		return (point.x() >= rectleft) && (point.x() <= rectright) && (point.y() >= recttop) && (point.y() <= rectbottom);
 	}
@@ -18,7 +18,7 @@ namespace DSS {
 	// The check has been copied literally. Even if it seems to be a bug (as DSSRect right and bottom are exclusive), we leave it as it is for the moment.
 	// CPoint had indices LONG x, LONG y.
 	//
-	bool blockContainsPixel(const DSSRect& block, const QPoint& pixel) noexcept
+	inline bool blockContainsPixel(const DSSRect& block, const QPoint& pixel) noexcept
 	{
 		return pixel.x() >= block.getLeft() && pixel.x() <= block.getRight() && pixel.y() >= block.getTop() && pixel.y() <= block.getBottom();
 	}
