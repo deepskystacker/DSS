b7bb1d07819e71917d02c4d249695a1a61fe512d
David C. Partridge <david.partridge@perdrix.co.uk>
2023-03-19 15:40:12 +0000

    Improve handling of invalid input data for scale factors on RAW/FITS DDP setttings and also for Dark Multiplication Factor and kappa input fields in Stacking Settings

--- DeepSkyStacker/RegisterSettings.h
+++ DeepSkyStacker/RegisterSettings.h
@@ -94,7 +94,7 @@ private slots:
 	void on_forceRegister_stateChanged(int);
 	void on_hotPixels_stateChanged(int);
 	void on_stackAfter_clicked();
-	void on_percentStack_textEdited(const QString &text);
+	void on_percentStack_editingFinished(const QString &text);
 
 	void on_luminanceThreshold_valueChanged(int);
 	void on_computeDetectedStars_clicked();
