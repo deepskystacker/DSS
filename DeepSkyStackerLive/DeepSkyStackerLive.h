835e8c079858777a45822487f70de7477abf2be4
David C. Partridge <david.partridge@perdrix.co.uk>
2023-03-31 12:47:07 +0100

    Start work converting DSSLive to Qt

--- DeepSkyStackerLive/DeepSkyStackerLive.h
+++ DeepSkyStackerLive/DeepSkyStackerLive.h
@@ -1,6 +1,82 @@
 #pragma once
-// DeepSkyStackerLive.h : main header file for the PROJECT_NAME application
+/****************************************************************************
+**
+** Copyright (C) 2020, 2022 David C. Partridge
+**
+** BSD License Usage
+** You may use this file under the terms of the BSD license as follows:
+**
+** "Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are
+** met:
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in
+**     the documentation and/or other materials provided with the
+**     distribution.
+**   * Neither the name of DeepSkyStacker nor the names of its
+**     contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+**
+**
+****************************************************************************/
+// DeepSkyStackerLive.h : main header file for DeepSkyStackerLive
 //
+#include "dssbase.h"
+
+class QWinHost;
+
+class DeepSkyStackerLive :
+	public QMainWindow,
+	public DSSBase
+{
+	typedef QMainWindow
+		Inherited;
+
+	Q_OBJECT
+
+public:
+	DeepSkyStackerLive();
+	~DeepSkyStackerLive();
+
+	//
+	// Don't intend this to be copied or assigned.
+	//
+	DeepSkyStackerLive(const DeepSkyStackerLive&) = delete;
+	DeepSkyStackerLive& operator=(const DeepSkyStackerLive&) = delete;
+	DeepSkyStackerLive(DeepSkyStackerLive&& rhs) = delete;
+	DeepSkyStackerLive& operator=(DeepSkyStackerLive&& rhs) = delete;
+
+	inline qreal pixelRatio() { return devicePixelRatioF(); }
+
+protected slots:
+	void updateStatus(const QString& text);
+	void displayMessage(const QString& message, QMessageBox::Icon icon);
+
+private:
+	bool initialised;
+	QWinHost* winHost;
+	QStringList args;
+	QString baseTitle;
+	QLabel* statusBarText;
+
+	void createStatusBar();
+};
+
 class CDeepSkyStackerLiveApp : public CWinApp
 {
 public:
