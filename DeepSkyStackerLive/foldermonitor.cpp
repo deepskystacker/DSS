bdaa0fa964c2c711a04ae4c4583f3a4a60c096a0
David C. Partridge <david.partridge@perdrix.co.uk>
2023-05-10 17:37:09 +0100

    FolderMonitor class

--- DeepSkyStackerLive/foldermonitor.cpp
+++ DeepSkyStackerLive/foldermonitor.cpp
@@ -6,8 +6,28 @@ namespace DSS
 {
 	FolderMonitor::FolderMonitor(QObject* parent)
 		: QObject(parent),
-		stopped{ false }
-	{}
+		stopped{ false },
+		delay{ 5UL }
+	{
+		QSettings settings;
+		settings.beginGroup("DeepSkyStackerLive");
+		QString folder = settings.value("MonitoredFolder", "").toString();
+		ZASSERT(!folder.isEmpty());
+
+		folderToWatch = folder.toStdU16String();
+		for (auto& file : std::filesystem::directory_iterator(path_to_watch))
+		{
+			//
+			// Only interested in reqular files.
+			//
+			if (file.is_regular_file())
+			{
+				existing_.push_back(file);
+				paths_[file] = std::filesystem::last_write_time(file);
+			}
+		}
+		emit existingFiles(existing_);
+	}
 
 	FolderMonitor::~FolderMonitor()
 	{}
@@ -15,8 +35,47 @@ namespace DSS
 	void FolderMonitor::run()
 	{
 		while (!stopped)
-		{
 
+		{
+			// Wait for "delay" milliseconds
+			QThread::sleep(delay);
+
+			auto it = paths_.begin();
+			while (it != paths_.end())
+			{
+				if (!std::filesystem::exists(it->first)) 
+				{
+					emit fileErased(it->first);
+					it = paths_.erase(it);
+				}
+				else
+				{
+					it++;
+				}
+			}
+
+			// Check if a file was created or modified
+			for (auto& file : std::filesystem::directory_iterator(folderToWatch))
+			{
+				auto current_file_last_write_time = std::filesystem::last_write_time(file);
+
+				if (!paths_.contains(file))
+				{
+					// File creation
+					paths_[file] = current_file_last_write_time;
+					emit fileCreated(file);
+					// File modification
+				}
+				else
+				{
+					// File modification
+					if (paths_[file] != current_file_last_write_time)
+					{
+						paths_[file] = current_file_last_write_time;
+						emit fileChanged(file);
+					}
+				}
+			}
 		}
 	}
 
--- DeepSkyStackerLive/foldermonitor.h
+++ DeepSkyStackerLive/foldermonitor.h
@@ -13,10 +13,30 @@ namespace DSS
 		FolderMonitor(QObject* parent);
 		~FolderMonitor();
 
+		//
+		// Don't intend this to be copied or assigned.
+		//
+		FolderMonitor(const FolderMonitor&) = delete;
+		FolderMonitor& operator=(const FolderMonitor&) = delete;
+		FolderMonitor(FolderMonitor&& rhs) = delete;
+		FolderMonitor& operator=(FolderMonitor&& rhs) = delete;
+
+
 	public slots:
 		void stop();
 
 	private:
-		std::atomic_bool stopped;
+		bool stopped;
+		unsigned long delay;
+		std::u16string folderToWatch;
+		std::unordered_map<fs::path, fs::file_time_type> paths_;
+		std::vector<fs::path> existing_;
+
+	signals:
+		void existingFiles(const std::vector<fs::path>&);
+		void fileChanged(const fs::path& file);
+		void fileCreated(const fs::path& file);
+		void fileErased(const fs::path& file);
+
 	};
 }
