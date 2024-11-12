4c8dfa7791d71d741a87b039aad3aff2c75967c1
Martin Toeltsch <martin.toeltsch@toeltsch.at>
2024-11-12 12:47:52 +0100

    Changes after Simon's review for OSX compilation.

--- DeepSkyStackerKernel/ExceptionHandling.cpp
+++ DeepSkyStackerKernel/ExceptionHandling.cpp
@@ -156,7 +156,9 @@ int addr2line(char const* const program_name, void const* const addr)
 	{
 		writeOutput(buff);
 	}
-	return WEXITSTATUS(pclose(in));
+
+	int status = pclose(in);
+	return WEXITSTATUS(status);
 }
 
 constexpr size_t MAX_STACK_FRAMES{ 64 };
