d504982c8b6dbdb494da2019ba309c2f4cdc6be0
Martin Toeltsch <martin.toeltsch@toeltsch.at>
2024-11-29 11:21:43 +0100

    Extract non-AVX code out of avx files to avoid ILLIGAL_INSTRUCTION under Linux (due to compiler flag -mavx2).

---
+++ DeepSkyStackerKernel/avx_1.cpp
@@ -0,0 +1,105 @@
+#include "stdafx.h"
+#include "avx.h"
+#include "avx_support.h"
+#include "avx_entropy.h"
+#include "PixelTransform.h"
+#include "TaskInfo.h"
+#include "EntropyInfo.h"
+#include "BackgroundCalibration.h"
+#include "dssrect.h"
+
+
+// ****************
+// Non-AVX Stacking
+// ****************
+
+int NonAvxStacking::stack(const CPixelTransform& pixelTransformDef, const CTaskInfo& taskInfo, const CBackgroundCalibration& backgroundCalibrationDef, std::shared_ptr<CMemoryBitmap> outputBitmap, const int pixelSizeMultiplier)
+{
+	const int width = this->stackData.width;
+	PIXELDISPATCHVECTOR vPixels;
+	vPixels.reserve(16);
+
+	const bool isColor = AvxSupport{ this->stackData.entropyData.inputBitmap }.isColorBitmapOrCfa();
+
+	for (int j = this->stackData.lineStart; j < this->stackData.lineEnd; ++j)
+	{
+		for (int i = 0; i < width; ++i)
+		{
+			const QPointF ptOut = pixelTransformDef.transform(QPointF(i, j));
+
+			COLORREF16 crColor;
+			double fRedEntropy = 1.0, fGreenEntropy = 1.0, fBlueEntropy = 1.0;
+
+			if (taskInfo.m_Method == MBP_ENTROPYAVERAGE)
+				this->stackData.entropyData.entropyInfo.GetPixel(i, j, fRedEntropy, fGreenEntropy, fBlueEntropy, crColor);
+			else
+				this->stackData.inputBitmap.GetPixel16(i, j, crColor);
+
+			float Red = crColor.red;
+			float Green = crColor.green;
+			float Blue = crColor.blue;
+
+			if (backgroundCalibrationDef.m_BackgroundCalibrationMode != BCM_NONE)
+				backgroundCalibrationDef.ApplyCalibration(Red, Green, Blue);
+
+			if ((0 != Red || 0 != Green || 0 != Blue) && DSSRect { 0, 0, this->stackData.resultWidth, this->stackData.resultHeight }.contains(ptOut))
+			{
+				vPixels.resize(0);
+				ComputePixelDispatch(ptOut, pixelSizeMultiplier, vPixels);
+
+				for (CPixelDispatch& Pixel : vPixels)
+				{
+					// For each plane adjust the values
+					if (Pixel.m_lX >= 0 && Pixel.m_lX < this->stackData.resultWidth && Pixel.m_lY >= 0 && Pixel.m_lY < this->stackData.resultHeight)
+					{
+						// Special case for entropy average
+						if (taskInfo.m_Method == MBP_ENTROPYAVERAGE)
+						{
+							if (isColor)
+							{
+								double fOldRed, fOldGreen, fOldBlue;
+
+								this->stackData.entropyData.pEntropyCoverage->GetValue(Pixel.m_lX, Pixel.m_lY, fOldRed, fOldGreen, fOldBlue);
+								fOldRed += Pixel.m_fPercentage * fRedEntropy;
+								fOldGreen += Pixel.m_fPercentage * fGreenEntropy;
+								fOldBlue += Pixel.m_fPercentage * fBlueEntropy;
+								this->stackData.entropyData.pEntropyCoverage->SetValue(Pixel.m_lX, Pixel.m_lY, fOldRed, fOldGreen, fOldBlue);
+
+								outputBitmap->GetValue(Pixel.m_lX, Pixel.m_lY, fOldRed, fOldGreen, fOldBlue);
+								fOldRed += Red * Pixel.m_fPercentage * fRedEntropy;
+								fOldGreen += Green * Pixel.m_fPercentage * fGreenEntropy;
+								fOldBlue += Blue * Pixel.m_fPercentage * fBlueEntropy;
+								outputBitmap->SetValue(Pixel.m_lX, Pixel.m_lY, fOldRed, fOldGreen, fOldBlue);
+							}
+							else
+							{
+								double fOldGray;
+
+								this->stackData.entropyData.pEntropyCoverage->GetValue(Pixel.m_lX, Pixel.m_lY, fOldGray);
+								fOldGray += Pixel.m_fPercentage * fRedEntropy;
+								this->stackData.entropyData.pEntropyCoverage->SetValue(Pixel.m_lX, Pixel.m_lY, fOldGray);
+
+								outputBitmap->GetValue(Pixel.m_lX, Pixel.m_lY, fOldGray);
+								fOldGray += Red * Pixel.m_fPercentage * fRedEntropy;
+								outputBitmap->SetValue(Pixel.m_lX, Pixel.m_lY, fOldGray);
+							}
+						}
+
+						double fPreviousRed, fPreviousGreen, fPreviousBlue;
+
+						this->stackData.tempBitmap.GetPixel(Pixel.m_lX, Pixel.m_lY, fPreviousRed, fPreviousGreen, fPreviousBlue);
+						fPreviousRed += static_cast<double>(Red) / 256.0 * Pixel.m_fPercentage;
+						fPreviousGreen += static_cast<double>(Green) / 256.0 * Pixel.m_fPercentage;
+						fPreviousBlue += static_cast<double>(Blue) / 256.0 * Pixel.m_fPercentage;
+						fPreviousRed = std::min(fPreviousRed, 255.0);
+						fPreviousGreen = std::min(fPreviousGreen, 255.0);
+						fPreviousBlue = std::min(fPreviousBlue, 255.0);
+						this->stackData.tempBitmap.SetPixel(Pixel.m_lX, Pixel.m_lY, fPreviousRed, fPreviousGreen, fPreviousBlue);
+					}
+				}
+			}
+		}
+	}
+
+	return 0;
+}
---
+++ DeepSkyStackerKernel/avx_histogram_1.cpp
@@ -0,0 +1,157 @@
+#include "stdafx.h"
+#include "avx_histogram.h"
+#include "MemoryBitmap.h"
+#include "ColorHelpers.h"
+
+
+// *****************
+// Non-AVX Histogram
+// *****************
+
+int NonAvxHistogram::calcHistogram(const size_t lineStart, const size_t lineEnd, const double multiplier)
+{
+	const size_t width = histoData.inputBitmap.Width();
+	const double fMultiplier = multiplier * 256.0;
+
+	for (size_t row = lineStart; row < lineEnd; ++row)
+	{
+		for (size_t col = 0; col < width; ++col)
+		{
+			double fRed, fGreen, fBlue;
+			this->histoData.inputBitmap.GetPixel(col, row, fRed, fGreen, fBlue);
+
+			constexpr auto ColorToIndex = [](const double color) {
+				constexpr double Maxvalue = static_cast<double>(std::numeric_limits<std::uint16_t>::max());
+				return static_cast<size_t>(std::min(color, Maxvalue));
+				};
+
+			++this->histoData.redHisto[ColorToIndex(fRed * fMultiplier)];
+			++this->histoData.greenHisto[ColorToIndex(fGreen * fMultiplier)];
+			++this->histoData.blueHisto[ColorToIndex(fBlue * fMultiplier)];
+		}
+	}
+
+	return 0;
+}
+
+
+// ------------------------
+// Non AVX Bezier functions
+// ------------------------
+
+int NonAvxBezierAndSaturation::avxAdjustRGB(const int nBitmaps, const class DSS::RGBHistogramAdjust& histoAdjust)
+{
+	const float scale = 255.0f / static_cast<float>(nBitmaps);
+
+	for (size_t n = 0, bufferLen = this->histoData.redBuffer.size(); n < bufferLen; ++n)
+	{
+		this->histoData.redBuffer[n] *= scale;
+		this->histoData.greenBuffer[n] *= scale;
+		this->histoData.blueBuffer[n] *= scale;
+	}
+	for (size_t n = 0, bufferLen = this->histoData.redBuffer.size(); n < bufferLen; ++n)
+	{
+		double r = this->histoData.redBuffer[n];
+		double g = this->histoData.greenBuffer[n];
+		double b = this->histoData.blueBuffer[n];
+		histoAdjust.Adjust(r, g, b);
+
+		this->histoData.redBuffer[n] = static_cast<float>(r);
+		this->histoData.greenBuffer[n] = static_cast<float>(g);
+		this->histoData.blueBuffer[n] = static_cast<float>(b);
+	}
+
+	constexpr size_t VecLen = sizeof(__m128) / sizeof(float);
+	constexpr float scalingFactor = static_cast<float>(1.0 / 255.0);
+
+	// SSE2 code is OK, because every x64 CPU supports it.
+	const auto loadAndScale = [scalingVector = _mm_set1_ps(scalingFactor), minVal = _mm_set1_ps(255.0f)](const float* pValue) -> __m128 {
+		return _mm_min_ps(_mm_mul_ps(_mm_loadu_ps(pValue), scalingVector), minVal);
+	};
+
+	for (size_t n = 0, nVecs = this->histoData.redBuffer.size() / VecLen; n < nVecs; ++n)
+	{
+		__m128 r = loadAndScale(this->histoData.redBuffer.data() + n * 4);
+		__m128 g = loadAndScale(this->histoData.greenBuffer.data() + n * 4);
+		__m128 b = loadAndScale(this->histoData.blueBuffer.data() + n * 4);
+		_mm_storeu_ps(this->histoData.redBuffer.data() + n * 4, r);
+		_mm_storeu_ps(this->histoData.greenBuffer.data() + n * 4, g);
+		_mm_storeu_ps(this->histoData.blueBuffer.data() + n * 4, b);
+	}
+	for (size_t n = (this->histoData.redBuffer.size() / VecLen) * VecLen, bufferLen = this->histoData.redBuffer.size(); n < bufferLen; ++n)
+	{
+		this->histoData.redBuffer[n] = std::min(this->histoData.redBuffer[n] * scalingFactor, 255.0f);
+		this->histoData.greenBuffer[n] = std::min(this->histoData.greenBuffer[n] * scalingFactor, 255.0f);
+		this->histoData.blueBuffer[n] = std::min(this->histoData.blueBuffer[n] * scalingFactor, 255.0f);
+	}
+
+	return 0;
+}
+
+int NonAvxBezierAndSaturation::avxToHsl()
+{
+	for (size_t n = 0, bufferLen = this->histoData.redBuffer.size(); n < bufferLen; ++n)
+	{
+		double h, s, l;
+		ToHSL(this->histoData.redBuffer[n], this->histoData.greenBuffer[n], this->histoData.blueBuffer[n], h, s, l);
+		this->histoData.redBuffer[n] = h;
+		this->histoData.greenBuffer[n] = s;
+		this->histoData.blueBuffer[n] = l;
+	}
+	return 0;
+}
+
+int NonAvxBezierAndSaturation::avxBezierAdjust(const size_t)
+{
+	for (size_t n = 0, bufferLen = this->histoData.blueBuffer.size(); n < bufferLen; ++n)
+	{
+		const auto it = std::lower_bound(this->histoData.bezierX.cbegin(), this->histoData.bezierX.cend(), this->histoData.blueBuffer[n]);
+		const std::ptrdiff_t ndx = it - this->histoData.bezierX.cbegin();
+		if (ndx < static_cast<std::ptrdiff_t>(this->histoData.bezierX.size()))
+			this->histoData.blueBuffer[n] = this->histoData.bezierY[ndx];
+	}
+	return 0;
+}
+
+int NonAvxBezierAndSaturation::avxBezierSaturation(const size_t, const float saturationShift)
+{
+	if (saturationShift == 0)
+		return 0;
+
+	const auto satShift = [shiftVal = saturationShift > 0 ? 10.0f / saturationShift : -0.1f * saturationShift](const float v) -> float
+	{
+		return std::pow(v, shiftVal);
+	};
+
+	std::transform(std::cbegin(this->histoData.greenBuffer), std::cend(this->histoData.greenBuffer), std::begin(this->histoData.greenBuffer), satShift);
+
+	return 0;
+}
+
+int NonAvxBezierAndSaturation::avxToRgb(const bool markOverAndUnderExposure)
+{
+	for (size_t n = 0, bufferLen = this->histoData.redBuffer.size(); n < bufferLen; ++n)
+	{
+		const float l = this->histoData.blueBuffer[n];
+
+		double r, g, b;
+		ToRGB(this->histoData.redBuffer[n], this->histoData.greenBuffer[n], l, r, g, b);
+
+		if (markOverAndUnderExposure)
+		{
+			const bool notoverexposed = l <= 1.0f;
+			const bool notunderexposed = l > (2.0f / 255.0f);
+
+			this->histoData.redBuffer[n] = notoverexposed ? (notunderexposed ? r : 0.0f) : 255.0f;
+			this->histoData.greenBuffer[n] = (notoverexposed && notunderexposed) ? g : 0.0f;
+			this->histoData.blueBuffer[n] = notunderexposed ? (notoverexposed ? b : 0.0f) : 255.0f;
+		}
+		else
+		{
+			this->histoData.redBuffer[n] = r;
+			this->histoData.greenBuffer[n] = g;
+			this->histoData.blueBuffer[n] = b;
+		}
+	}
+	return 0;
+}
