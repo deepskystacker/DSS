17c0e380c9a24dc17cd8514a72b81e5fc0fa6f8d
David C. Partridge <david.partridge@perdrix.co.uk>
2025-03-13 16:31:59 +0000

    Changes for 5.1.9

--- DeepSkyStackerKernel/DSSVersion.h
+++ DeepSkyStackerKernel/DSSVersion.h
@@ -10,7 +10,7 @@ constexpr int DSSBETAEXPIREMONTH = 5;
 
 #define DSSVER_MAJOR			5
 #define DSSVER_MINOR			1
-#define DSSVER_SUB				8
+#define DSSVER_SUB				9
 
 #ifdef DSSBETA
 #define DSSVER_BUILD			DSSBETARELEASE
