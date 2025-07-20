a5db47a86b07f0f41b360706bab3a6e6e24b6ca4
David C. Partridge <david.partridge@perdrix.co.uk>
2025-07-20 15:12:57 +0100

    Enable reportError to invoke QErrorMessage as a modal dialogue.

--- DeepSkyStackerKernel/dssbase.h
+++ DeepSkyStackerKernel/dssbase.h
@@ -65,7 +65,7 @@ public:
 	// severity 2 is QMessageBox::Warning
 	virtual void reportError(const QString& message, const QString& type,
 		Severity severity = Severity::Warning, Method method = Method::QMessageBox,
-		bool terminate = false) = 0;
+		bool terminate = false, Qt::ConnectionType connectionType = Qt::ConnectionType::AutoConnection) = 0;
 
 	inline static DSSBase* instance()
 	{
