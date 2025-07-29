5a528d49b7b610af62086c00605f53b373694da5
David C. Partridge <david.partridge@perdrix.co.uk>
2025-07-29 09:16:33 +0100

    Copyright statements should attribute to Martin Toeltsch

--- DeepSkyStackerKernel/avx.h
+++ DeepSkyStackerKernel/avx.h
@@ -1,7 +1,7 @@
 #pragma once 
 /****************************************************************************
 **
-** Copyright (C) 2020, 2025 David C. Partridge
+** Copyright (C) 2024, 2025 Martin Toeltsch
 **
 ** BSD License Usage
 ** You may use this file under the terms of the BSD license as follows:
