0cd20359489d46eed1c5aa6e2acc7a1f6d650929
David C. Partridge <david.partridge@perdrix.co.uk>
2025-11-23 09:53:26 +0000

    Remove csa and nn code

--- Tools/nn/config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#if defined(_WIN32)
-//#define isnan _isnan
-#define copysign _copysign
-#define rint (int)
-#define M_PI 3.14159265358979323846
-#define TRILIBRARY
-#define NO_TIMER
-#endif
--- Tools/nn/nnpi.h
+++
@@ -1,4 +0,0 @@
-void nnpi_calculate_weights(nnpi* nn, point* p);
-int nnpi_get_nvertices(nnpi* nn);
-int* nnpi_get_vertices(nnpi* nn);
-double* nnpi_get_weights(nnpi* nn);
