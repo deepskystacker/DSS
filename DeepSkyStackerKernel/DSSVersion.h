04773b6d898e534ef6945ce711d4e4ab8e06e702
David C. Partridge <david.partridge@perdrix.co.uk>
2026-02-06 16:34:31 +0000

    Update copyright

--- DeepSkyStackerKernel/DSSVersion.h
+++ DeepSkyStackerKernel/DSSVersion.h
@@ -39,7 +39,7 @@ constexpr int DSSBETAEXPIREMONTH = 6;
 #define DSSVER_FILEVERSION		VERFILESTRING(DSSVER_MAJOR, DSSVER_MINOR, DSSVER_SUB, DSSVER_BUILD)
 #define DSSVER_PRODUCTVERSION	VERFILESTRING(DSSVER_MAJOR, DSSVER_MINOR, DSSVER_SUB, DSSVER_BUILD)
 
-#define DSSVER_COPYRIGHT "Copyright © 2018-2025 David C. Partridge, Tony Cook, Mat Draper, Simon C. Smith, Vitali Pelenjow,\
+#define DSSVER_COPYRIGHT "Copyright © 2018-2026 David C. Partridge, Tony Cook, Mat Draper, Simon C. Smith, Vitali Pelenjow,\
 Tomas Tatara, Michal Schulz, Martin Toeltsch, Iustin Amihaesei;\n\
 Copyright © 2006-2019 Luc Coiffier"
 	
