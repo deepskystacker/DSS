a44ff4ba105dbfa004d14db9eb312d829ce5cb50
David C. Partridge <david.partridge@perdrix.co.uk>
2026-07-15 07:34:55 +0100

    Move sse2 colour conversion code to ColorHelpers

--- DeepSkyStackerKernel/ColorHelpers.h
+++ DeepSkyStackerKernel/ColorHelpers.h
@@ -32,3 +32,33 @@ std::tuple<double, double, double> hslToRgb(double h, double s, double l);
 //double GetLuminance(const COLORREF crColor);	// <--- Don't this this is used.
 double GetLuminance(const COLORREF16& crColor);
 double ClampPixel(const double fValue);
+
+//
+// Convert vectors of RGB values to HSL vectorized with SSE2 (4 floats per loop).
+// r, g, b expected in [0, 1].
+// 
+// Returns h in degrees [0,360), s and l in [0,1].
+//
+// On input redBuffer, greenBuffer, blueBuffer contain R,G,B values, on output they contain H,S,L values.
+// 
+// There's no need to have fall back code for the last few pixels, as the vector size of CGrayBitmap
+// is always a multiple of 32 bytes (8 floats).   This works perfectly for __m128 and __m256 types.
+// If we ever decide to use __m512 types we will need to revisit the over-allocation in CGrayBitmapT<T>.
+// 
+// See CGrayBitmapT<T>::InitInternals() for details
+// 
+void rgbToHsl_sse2(std::vector<float>& redBuffer, std::vector<float>& greenBuffer, std::vector<float>& blueBuffer);
+
+// H,S,L -> R,G,B vectorized with SSE2 (4 floats per loop).
+// h: degrees in [0,360) (per-pixel), s,l in [0,1].
+// r,g,b outputs in [0,1].
+// 
+// On input redBuffer, greenBuffer, blueBuffer contain H,S,L values, on output they contain R,G,B values.
+// 
+// There's no need to have fall back code for the last few pixels, as the vector size of CGrayBitmap
+// is always a multiple of 32 bytes (8 floats).   This works perfectly for __m128 and __m256 types.
+// If we ever decide to use __m512 types we will need to revisit the over-allocation in CGrayBitmapT<T>.
+// 
+// See CGrayBitmapT<T>::InitInternals() for details
+//
+void hslToRgb_sse2(std::vector<float>& redBuffer, std::vector<float>& greenBuffer, std::vector<float>& blueBuffer);
